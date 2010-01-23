0c4042b6afb62ad99926963dfff78618106e7f65
Tyler Hunt <tyler@tylerhunt.com>
2010-01-23 14:12:34 -0500

    Switched to transactional tests.
    Should perform better than auto-migrating before every test.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,13 +14,28 @@ Webrat.configure do |config|
   config.mode = :rack
 end
 
+DataMapper.auto_migrate!
+
 Spec::Runner.configure do |config|
   include Rack::Test::Methods
   include Webrat::Methods
   include Webrat::Matchers
 
   config.before(:each) do
-    DataMapper.auto_migrate!
+    repository(:default) do
+      transaction = DataMapper::Transaction.new(repository)
+      transaction.begin
+      repository.adapter.push_transaction(transaction)
+    end
+  end
+
+  config.after(:each) do
+    repository(:default) do
+      while repository.adapter.current_transaction
+        repository.adapter.current_transaction.rollback
+        repository.adapter.pop_transaction
+      end
+    end
   end
 
   def app
